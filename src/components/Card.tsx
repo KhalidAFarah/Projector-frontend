d0b2953ae384f986bab71652a342d09500806e72
KhalidAFarah <56063705+KhalidAFarah@users.noreply.github.com>
2021-11-09 13:31:21 +0100

    added typescript

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,16 +1,12 @@
-import React from 'react'
-import { Row } from 'react-bootstrap';
-import testingimage from "../testimage.PNG";
 
-
-const Card = (props) => {
+const Card = (props:any) => {
 
    
 
     return (
         <div className="Card col" style={{paddingLeft: 3+"rem", paddingRight: 3+"rem"}} onClick={props.clickfunc}>
             <div className="card h-100 border-dark" style={{transitionDuration: 0.2+"s"}}>
-                <img src={testingimage} alt="sup" className="card-img-top"/>
+                <img src="../../public/" alt="sup" className="card-img-top"/>
                 <div className="card-body Card-body" >
                     <h4 className="card-title text-start">{props.title}</h4>
                     <p className="card-text text-start fs-5 p-2">{props.txt}</p>
