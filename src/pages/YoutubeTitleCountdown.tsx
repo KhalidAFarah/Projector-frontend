89d575294d15c1c43880f00a23c6fd3030296531
KhalidAFarah <56063705+KhalidAFarah@users.noreply.github.com>
2022-06-17 18:22:32 +0200

    Livestream update #10 View has neccesary componenets and almost all necessary features features

--- src/pages/YoutubeTitleCountdown.tsx
+++ src/pages/YoutubeTitleCountdown.tsx
@@ -2,7 +2,7 @@
 
 import { gapi } from "gapi-script";
 import { useEffect, useState } from "react";
-import { Button } from "react-bootstrap";
+import { Button, Form, Row, Card, Col } from "react-bootstrap";
 import GoogleLogin from "react-google-login";
 
 interface AuthResponse {
@@ -43,6 +43,15 @@ const YoutubeTitleCountdown = () => {
 
   const [token, setToken] = useState("");
   const [next, setNext] = useState("");
+
+  const def:any[] = []
+  const [videos, setVideos] = useState([]);
+  const [shownVideos, setShownVideos] = useState([]);
+  const [chosen, setChosen] = useState(def);
+
+  const [hour, setHour] = useState(0);
+  const [min, setMin] = useState(0);
+
   const onSuccess = async (res: any) => {
     setToken(res.tokenObj.access_token)
     //console.log(res)
@@ -65,20 +74,28 @@ const YoutubeTitleCountdown = () => {
       console.log(data)
 
       setNext(data.nextPageToken)
+
+      setVideos(data.items);
+      setShownVideos(data.items);
     }).catch((error) => {
       console.log(error)
     })
   };
 
   return (
-    <div className="h-screen w-screen flex items-center justify-center flex-col">
+  <>
+    <Row>
+
+    <Col>
+    
       
         <GoogleLogin
           clientId={`${process.env.REACT_APP_YT_CLIENT_ID}`}
           onSuccess={onSuccess}
         />
+        </Col>
       
-
+        <Col>
       {token != "" && (
         <Button variant="primary" onClick={()=>{
           if(next != "") {
@@ -102,8 +119,84 @@ const YoutubeTitleCountdown = () => {
         }}>Show more</Button>
       )}
 
+</Col>
+
+<Col>
+
+        <Form.Control type="text" onChange={(e:any) => {
+
+          const searchedVideos = videos.filter((video:any) => video.snippet.title.toLowerCase().includes(e.target.value.toLowerCase()))
+          setShownVideos(searchedVideos)
+        }}/>
+
+</Col>
+<Col>
+        <Button variant="primary" onClick={() => {
+          fetch("/api/youtube/?hour="+hour+"&min="+min,{
+            method: "POST",
+            headers: {
+              'Content-Type': 'application/json',
+              'Accept': 'application/json',
+            },
+            body: JSON.stringify(chosen)
+          }).then((response) => {
+            if(response.ok){
+              return response.json();
+            }
+            throw response;
+          }).then( (data) => {
+            console.log(data)
+          }).catch((error) => {
+            console.log(error)
+          })
+        }}>Submit</Button>
+
+</Col>
+<Col>
+<Form.Control type="number" onChange={(e:any) => {
+  setHour(e.target.value)
+}}/>
+</Col>
+
+<Col>
+<Form.Control type="number" onChange={(e:any) => {
+  setMin(e.target.value)
+}}/>
+
+</Col>
       
-    </div>
+    </Row>
+    <Row>
+      {shownVideos.map((video:any, index:number) => (
+        <Card onClick={(e:any)=> {
+
+          if(e.target.style != "Border: solid #0d6efd 0.1rem"){
+            e.target.style = "Border: solid #0d6efd 0.1rem";
+            setChosen([...chosen, video]);
+          }else{
+            e.target.style = "";
+            const t = chosen.filter((element) => element != video);
+            console.log(t)
+            setChosen(t);
+          }
+            
+
+          //console.log(chosen)
+
+
+        }} key={index} style={{ width: '18rem', margin: "1rem" }}>
+          <Card.Img variant="top" src={video.snippet.thumbnails.medium.url} />
+          <Card.Body>
+            <Card.Title>{video.snippet.title}</Card.Title>
+            <Card.Text>
+              
+            </Card.Text>
+            
+          </Card.Body>
+        </Card>
+      ))}
+    </Row>
+    </>
   );
 };
 export default YoutubeTitleCountdown
